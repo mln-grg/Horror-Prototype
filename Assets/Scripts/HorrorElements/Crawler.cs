6688c64148ded2f6c8f8d2ab06e51e4a5dd482de
mrdocx <arjunchengaroor@gmail.com>
2021-01-16 12:07:14 +0530

    Crawl And Little Girl

---
+++ Assets/Scripts/HorrorElements/Crawler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crawler : MonoBehaviour
+{
+    public Transform pointA;
+    public float speed = 1;
+    public bool trigger;
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (trigger == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, pointA.position, speed * Time.deltaTime);
+        }
+        if (transform.position == pointA.position)
+        {
+            this.gameObject.SetActive(false);
+        }
+        
+    }
+}
